6e281ad869b065d89fb188f49a40d1f5e106c2be
YashBhalodi <yashbhalodi007@yahoo.com>
2019-09-20 20:04:47 +0530

    Made a mini game!

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'dart:math';
 
 void main() => runApp(MyApp());
 
@@ -23,14 +24,15 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-  bool isTicked = false;
+//  bool isTicked = false;
+  int totalScore = 0;
+  int targetNumber = Random().nextInt(9);
 
-  Widget tickedTarget(bool isSelected) {
+/*  Widget tickedTarget(bool isSelected) {
     return isSelected
         ? Container(
             width: 80.0,
             height: 80.0,
-            margin: EdgeInsets.all(10.0),
             decoration: BoxDecoration(
               color: Colors.green[50],
               border: Border.all(color: Colors.green[300], width: 1.0),
@@ -38,13 +40,12 @@ class _MyHomePageState extends State<MyHomePage> {
             child: Icon(
               Icons.check_circle_outline,
               color: Colors.green,
-              size: 80.0,
+              size: 40.0,
             ),
           )
         : Container(
             width: 80.0,
             height: 80.0,
-            margin: EdgeInsets.all(10.0),
             decoration: BoxDecoration(
               color: Colors.red[50],
               border: Border.all(color: Colors.red[300], width: 1.0),
@@ -52,7 +53,7 @@ class _MyHomePageState extends State<MyHomePage> {
             child: Icon(
               Icons.close,
               color: Colors.red,
-              size: 80.0,
+              size: 40.0,
             ),
           );
   }
@@ -68,7 +69,7 @@ class _MyHomePageState extends State<MyHomePage> {
             child: Icon(
               Icons.check,
               color: Colors.greenAccent[100],
-              size: 50.0,
+              size: 40.0,
             ),
           )
         : Container(
@@ -80,7 +81,7 @@ class _MyHomePageState extends State<MyHomePage> {
             child: Icon(
               Icons.close,
               color: Colors.redAccent[100],
-              size: 50.0,
+              size: 40.0,
             ),
           );
   }
@@ -103,6 +104,85 @@ class _MyHomePageState extends State<MyHomePage> {
               color: Colors.redAccent[100],
             ),
           );
+  }*/
+
+  Widget numberDrag(int i, double height, double width) {
+    return SizedBox(
+      width: width,
+      height: height,
+      child: Draggable(
+        child: Card(
+          elevation: 10.0,
+          child: Container(
+            color: Colors.blue[200],
+            width: width,
+            height: height,
+            child: Center(
+              child: Text(
+                i.toString(),
+                style: TextStyle(fontSize: 18.0),
+              ),
+            ),
+          ),
+        ),
+        feedback: Card(
+          elevation: 10.0,
+          child: Container(
+            color: Colors.blue[200],
+            width: width,
+            height: height,
+            child: Center(
+              child: Text(
+                i.toString(),
+                style: TextStyle(fontSize: 18.0),
+              ),
+            ),
+          ),
+        ),
+        data: i,
+        childWhenDragging: Container(),
+      ),
+    );
+  }
+
+  Widget numberTarget(int i, double width, double height) {
+    return SizedBox(
+      width: width,
+      height: height,
+      child: DragTarget(
+        onWillAccept: (data) {
+          if (data == i) {
+            return true;
+          } else {
+            setState(() {
+              totalScore--;
+            });
+            return false;
+          }
+        },
+        onAccept: (data) {
+          setState(() {
+            targetNumber = Random().nextInt(9);
+            totalScore++;
+          });
+        },
+        builder: (context, List<int> candidateData, rejectedData) {
+          return Center(
+            child: Container(
+              color: Colors.green[200],
+              width: width,
+              height: height,
+              child: Center(
+                child: Text(
+                  i.toString(),
+                  style: TextStyle(fontSize: 18.0),
+                ),
+              ),
+            ),
+          );
+        },
+      ),
+    );
   }
 
   @override
@@ -113,37 +193,60 @@ class _MyHomePageState extends State<MyHomePage> {
       ),
       body: Column(
         children: <Widget>[
-          Container(
-            child: DragTarget(
-              onWillAccept: (data) {
-                return true;
-              },
-              onAccept: (data) {
-                setState(() {
-                  isTicked = !isTicked;
-                });
-              },
-              builder: (context, List<int> candidateData, rejectedData) {
-                print(candidateData);
-                return Center(
-                  child: tickedTarget(isTicked),
-                );
-              },
+          Expanded(
+            child: Row(
+              mainAxisSize: MainAxisSize.min,
+              children: <Widget>[
+                numberTarget(targetNumber, 80.0, 80.0),
+              ],
             ),
           ),
-          Container(
-            height: 80.0,
-            width: 80.0,
-            child: Draggable(
-              data: 1,
-              child: tickButton(!isTicked),
-              feedback: tickDrag(!isTicked),
-              childWhenDragging: Padding(
-                padding: const EdgeInsets.all(8.0),
-                child: CircularProgressIndicator(),
+          Card(
+            elevation: 10.0,
+            child: Container(
+              width: 200.0,
+              height: 60.0,
+              child: Center(
+                child: Text(
+                  "Total Score: $totalScore.",
+                  style: TextStyle(fontSize: 20.0),
+                ),
               ),
             ),
           ),
+          Expanded(
+            child: Center(
+              child: Column(
+                mainAxisSize: MainAxisSize.min,
+                children: <Widget>[
+                  Row(
+                    mainAxisSize: MainAxisSize.min,
+                    children: <Widget>[
+                      numberDrag(1, 80.0, 80.0),
+                      numberDrag(2, 80.0, 80.0),
+                      numberDrag(3, 80.0, 80.0),
+                    ],
+                  ),
+                  Row(
+                    mainAxisSize: MainAxisSize.min,
+                    children: <Widget>[
+                      numberDrag(4, 80.0, 80.0),
+                      numberDrag(5, 80.0, 80.0),
+                      numberDrag(6, 80.0, 80.0),
+                    ],
+                  ),
+                  Row(
+                    mainAxisSize: MainAxisSize.min,
+                    children: <Widget>[
+                      numberDrag(7, 80.0, 80.0),
+                      numberDrag(8, 80.0, 80.0),
+                      numberDrag(9, 80.0, 80.0),
+                    ],
+                  ),
+                ],
+              ),
+            ),
+          )
         ],
       ),
     );
